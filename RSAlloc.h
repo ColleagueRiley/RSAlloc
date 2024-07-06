3ac887c1ca9f545064cba56e009ac666ef0cddf8
Colleague Riley <colleagueRiley@gmail.com>
2024-07-06 14:09:35 -0400

    bug fixes, add debug log, add mmap and VirtualAllocEx support

--- RSAlloc.h
+++ RSAlloc.h
@@ -26,12 +26,29 @@
 	(MAKE SURE RSA_IMPLEMENTATION is in exactly one header or you use -D RSA_IMPLEMENTATION)
 	#define RSA_IMPLEMENTATION - makes it so source code is included with header
     #define RSA_BSS - use BSS memory instead of malloc
+    #define RSA_DEBUG - show debug info
     #define RSA_MALLOC [x] / #define RSA_FREE [x] - define your own malloc/free functions to use
     #define RSA_DEFAULT_ARENA [x] - set default arena size
     #define RSA_CHUNK_COUNT [x] - set max chunk count
 */
 
-#ifndef RSA_BSS
+#if (defined(_WIN32) || defined(_WIN64))
+#define RSA_PRINT_U64 "%lli"
+#else
+#define RSA_PRINT_U64 "%li"
+#endif
+
+#ifdef RSA_DEBUG
+#include <stdio.h>
+#endif
+
+#if !defined(RSA_MALLOC) && !defined(RSA_BSS) && (defined(__linux__) || defined(__unix__) || defined(__APPLE__))
+#define RSA_MMAP
+
+#elif !defined(RSA_MALLOC) && !defined(RSA_BSS) && (defined(_WIN32) || defined(_WIN64))
+#define RSA_VIRTUAL_ALLOC
+
+#elif !defined(RSA_BSS) && !defined(RSA_MMAP) && !defined(RSA_VIRTUAL_ALLOC)
 #ifndef RSA_MALLOC
 #include <stdlib.h>
 #define RSA_MALLOC malloc
@@ -40,7 +57,7 @@
 #endif
 
 #ifndef RSA_DEFAULT_ARENA
-#define RSA_DEFAULT_ARENA 4000 /* ~ 4 KB */
+#define RSA_DEFAULT_ARENA 4194000 /* ~ 4 MB */
 #endif
 
 #ifndef RSA_CHUNK_COUNT
@@ -114,7 +131,7 @@ extern "C" {
 typedef struct RSA_chunk {
     size_t start;
     size_t len;
-    b8 used : 1;
+    b8 used;
 } RSA_chunk;
 
 RSADEF void RSA_init(size_t arenaSize);
@@ -127,6 +144,15 @@ RSADEF void RSA_deInit(void);
 
 #ifdef RSALLOC_IMPLEMENTATION
 
+#ifdef RSA_VIRTUAL_ALLOC
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#elif defined(RSA_MMAP)
+#include <sys/mman.h>
+#endif
+
+
 #ifndef RSA_BSS
 void* RSA_memory;
 #else
@@ -135,21 +161,59 @@ uintptr_t RSA_memory[RSA_DEFAULT_ARENA];
 
 RSA_chunk RSA_chunks[RSA_CHUNK_COUNT] = {{0}};
 size_t RSA_chunkLen = 0;
+size_t RSA_freedChunks = 0;
 
 size_t RSA_arenaSize = 0;
 
 void RSA_init(size_t arenaSize) {
-    RSA_arenaSize = arenaSize;
-
-    #ifdef RSA_BSS
-    if (RSA_arenaSize == 0)
+    #ifndef RSA_BSS
+    if (arenaSize == 0)
     #endif
     {
-        RSA_arenaSize = RSA_DEFAULT_ARENA;
+        arenaSize = RSA_DEFAULT_ARENA;
     }
     
-    #ifndef RSA_BSS
-    RSA_memory = malloc(arenaSize);
+    RSA_arenaSize = arenaSize;
+
+    #ifdef RSA_DEBUG
+    printf("RSA_INFO : init'ing arena with size " RSA_PRINT_U64 " bytes\n", RSA_arenaSize);
+    #endif
+
+    #ifdef RSA_MALLOC
+    RSA_memory = malloc(RSA_arenaSize);
+
+    #ifdef RSA_DEBUG
+    printf("RSA_INFO : init'd using malloc\n", RSA_memory);
+    #endif
+    #endif
+
+    #ifdef RSA_MMAP
+    RSA_memory = mmap(NULL, arenaSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+
+    #ifdef RSA_DEBUG
+    printf("RSA_INFO : init'd using unix's mmap\n");
+    #endif
+    #endif
+
+    #ifdef RSA_VIRTUAL_ALLOC
+    RSA_memory = VirtualAllocEx(GetCurrentProcess(), NULL, arenaSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+    #ifdef RSA_DEBUG
+    printf("RSA_INFO : init'd using window's VirtualAlloc\n");
+    #endif
+    #endif
+
+    #if defined(RSA_DEBUG) && defined(RSA_BSS)
+    printf("RSA_INFO : init'd using bss\n");
+    #endif
+
+    #ifdef RSA_DEBUG
+    printf("RSA_INFO : init'ed arena starting at %p\n", RSA_memory);
+    #endif
+
+    #if defined(RSA_DEBUG) && !RSA_BSS
+    if (RSA_memory == NULL)
+        printf("RSA_INFO : failed to allocate memory\n");
     #endif
 }
 
@@ -158,23 +222,38 @@ void* RSA_alloc(size_t size) {
 
     if (RSA_chunkLen) {
         size_t i = 0; 
-        
-        size_t found_start = 0;
-        size_t found_size = SIZE_MAX; 
-        
         for (i = 0; i < RSA_chunkLen; i++) {
             if (RSA_chunks[i].used) {
                 start = RSA_chunks[i].start + RSA_chunks[i].len;
             }
-            else if (RSA_chunks[i].len >= size && found_size > RSA_chunks[i].len) {
-                found_start = RSA_chunks[i].start;
-                found_size = RSA_chunks[i].len;
+            else if (RSA_chunks[i].len >= size) {
+                start = RSA_chunks[i].start;
+                
+                #ifdef RSA_DEBUG
+                    printf("RSA_INFO : Reusing free'd memory\n");
+                #endif
+
+                if (RSA_chunks[i].len - size || i == RSA_CHUNK_COUNT - 1) {
+                    RSA_chunks[i].start += size;
+                    RSA_chunks[i].len -= size;
+
+                    #ifdef RSA_DEBUG
+                    printf("RSA_INFO : Moving free chunk to %p\n", RSA_memory + start + size);
+                    #endif
+
+                    break;
+                }
+
+                #ifdef RSA_DEBUG
+                printf("RSA_INFO : Deleting dead chunk at %p\n", RSA_memory + start);
+                #endif
+
+                memcpy(RSA_chunks + 1, &RSA_chunks[i + 1], RSA_chunkLen - i);
+                RSA_chunks[RSA_CHUNK_COUNT - 1].used = 0;
+
+                break;
             }
         }    
-        
-        if (found_start) {
-            start = found_start;
-        }
     }
 
     if (start + size >= RSA_arenaSize)
@@ -185,6 +264,10 @@ void* RSA_alloc(size_t size) {
     RSA_chunks[RSA_chunkLen].used = 1;
     RSA_chunkLen++;
 
+    #ifdef RSA_DEBUG
+    printf("RSA_INFO : allocated " RSA_PRINT_U64 " sized chunk at %p\n", size, RSA_memory + start);
+    #endif
+
     return RSA_memory + start;
 }
 
@@ -194,7 +277,7 @@ void* RSA_calloc(size_t size, size_t typeSize) {
         return NULL;
 
     size_t i; 
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size * typeSize; i++) {
         ((u8*)ptr)[i] = 0;  
     }
 
@@ -208,19 +291,49 @@ void RSA_free(void* ptr) {
     for (i = 0; i < RSA_chunkLen; i++) {
         if (index == RSA_chunks[i].start) {
             RSA_chunks[i].used = 0;
-        }
-    }
+        
+            #ifdef RSA_DEBUG
+            printf("RSA_INFO : freed " RSA_PRINT_U64 " sized chunk at %p\n", RSA_chunks[i].len, RSA_memory + RSA_chunks[i].start);
+            #endif
 
-    for (i = RSA_chunkLen - 1; i > 0; i--) {
-        if (RSA_chunks[i].used == 0 && RSA_chunks[i + 1].used == 0) {
-            RSA_chunks[i].len += RSA_chunks[i + 1].len;
+            if (i && RSA_chunks[i - 1].used == 0) {
+                #ifdef RSA_DEBUG
+                printf("RSA_INFO : merging free chunk %p with %p to create a " RSA_PRINT_U64 " sized free chunk\n", 
+                    RSA_memory + RSA_chunks[i - 1].start,
+                    RSA_memory + RSA_chunks[i].start,
+                    RSA_chunks[i - 1].len + RSA_chunks[i].len
+                );
+                #endif
+
+                RSA_chunks[i - 1].len += RSA_chunks[i].len;
+                RSA_chunks[i].len = 0;
+                
+                memcpy(RSA_chunks + i, &RSA_chunks[i + 1], RSA_chunkLen - i);
+                RSA_chunks[RSA_CHUNK_COUNT - 1].used = 0;
+            }
+
+            #ifdef RSA_DEBUG
+            RSA_freedChunks += 1;
+            #endif
         }
     }
 }
 
 void RSA_deInit(void) {
-    #ifndef RSA_BSS
+    #ifdef RSA_MALLOC
     free(RSA_memory);
+    #endif
+
+    #ifdef RSA_MMAP
+    munmap(RSA_memory, RSA_arenaSize);
+    #endif
+
+    #ifdef RSA_VIRTUAL_ALLOC
+    VirtualFreeEx(GetCurrentProcess(), RSA_memory, 0, MEM_RELEASE);
+    #endif
+
+    #ifdef RSA_DEBUG
+    printf("RSA_INFO : DeInit with " RSA_PRINT_U64 " possible memory leaks\n", RSA_chunkLen - RSA_freedChunks);
     #endif
 
     RSA_chunkLen = 0;
